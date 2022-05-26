4733ec6622468e30bb28fe7a1789bdf2c9103c45
timved <timved@mail.ru>
2022-05-27 00:03:39 +0300

    add test

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	"github.com/BurntSushi/toml"
 	"github.com/timved/golang_rest_api/internal/app/apiserver"
 )
 
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
 func main() {
-	s := apiserver.New()
-	if err := s.Start(); err == nil {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+
+	_, err := toml.DecodeFile(configPath, config)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
 		log.Fatal(err)
 	}
 }
